dc5102df8bef1506b2a4879c1dd5d6aa74760c41
Laserzwei <lasernr2mail.com>
2021-08-01 15:35:28 +0200

    2.0 update
    removed not spawning asteroids modifier

--- data/scripts/entity/detectrespawningasteroids.lua
+++ data/scripts/entity/detectrespawningasteroids.lua
@@ -14,7 +14,8 @@ function DetectrespawnAsteroids.initialize()
 end
 
 function DetectrespawnAsteroids.onSectorEntered()
-    if Sector():hasScript("data/scripts/sector/background/respawnresourceasteroids.lua") then
+    local sector = Sector()
+    if sector:hasScript("data/scripts/sector/background/respawnresourceasteroids.lua") and #{sector:getEntitiesByType(EntityType.Asteroid)} > 0 then
         broadcastInvokeClientFunction("isRespawning")
     else
         broadcastInvokeClientFunction("isNotRespawning")
@@ -29,6 +30,6 @@ end
 
 function DetectrespawnAsteroids.isNotRespawning()
     removeShipProblem("respawning asteroids", Entity().id)
-    addShipProblem("no respawning asteroids", Entity().id, "Asteroids do not respawn in this Sector"%_t, "data/textures/icons/diamond.png", ColorRGB(1.0, 0.2, 0.2))
+    --addShipProblem("no respawning asteroids", Entity().id, "Asteroids do not respawn in this Sector"%_t, "data/textures/icons/diamond.png", ColorRGB(1.0, 0.2, 0.2))
 end
 
